fc4de7ed588f3f06863b2470dedae54af91a8de7
Thiago Araujo <161383143+Thiago-Arauj@users.noreply.github.com>
2024-05-30 14:27:53 -0300

    Finished Version

--- Batalha Naval.py
+++ Batalha Naval.py
@@ -1,20 +1,19 @@
 from random import randint
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Back
+from os import system
 import functools
-init()
+init(autoreset=True)
 
-simbol = ('✖','✦')
-
-grid = {'A': [x for x in range(1,11)],
-        'B': [x for x in range(1,11)], 
-        'C': [x for x in range(1,11)],
-        'D': [x for x in range(1,11)],
-        'E': [x for x in range(1,11)],
-        'F': [x for x in range(1,11)],
-        'G': [x for x in range(1,11)],
-        'H': [x for x in range(1,11)],
-        'I': [x for x in range(1,11)],
-        'J': [x for x in range(1,11)]}
+grid = {'A': [0,1,2,3,4,5,6,7,8,9,10],
+        'B': [0,1,2,3,4,5,6,7,8,9,10], 
+        'C': [0,1,2,3,4,5,6,7,8,9,10],
+        'D': [0,1,2,3,4,5,6,7,8,9,10],
+        'E': [0,1,2,3,4,5,6,7,8,9,10],
+        'F': [0,1,2,3,4,5,6,7,8,9,10],
+        'G': [0,1,2,3,4,5,6,7,8,9,10],
+        'H': [0,1,2,3,4,5,6,7,8,9,10],
+        'I': [0,1,2,3,4,5,6,7,8,9,10],
+        'J': [0,1,2,3,4,5,6,7,8,9,10]}
 
 # ===========================================================================================================
 # Funções que fazem a máquina colocar todos os barcos em posições aleatórias
@@ -127,7 +126,7 @@ def vertical_generator(size, row, x, column, ships, i):
             for colunas in range(column, C):
                     ships[i].append(f'{x}/{colunas}')
 
-        elif (C := column - size) >= 0:
+        elif (C := column - size) > 0:
             for colunas in range(C, column):
                 ships[i].append(f'{x}/{colunas}')
     
@@ -140,7 +139,7 @@ def horizontal_generator(size, row, x, column, ships, i):
         for colunas in range(column, C):
             ships[i].append(f'{x}/{colunas}')
 
-    elif (C := column - size) >= 0:
+    elif (C := column - size) > 0:
         for colunas in range(C, column):
             ships[i].append(f'{x}/{colunas}')
     else:
@@ -155,42 +154,118 @@ def horizontal_generator(size, row, x, column, ships, i):
  
 # ===========================================================================================================
 
-def grid_show(grid,barcos):
-
-    print('  ', end='')
-    for n in range(1,11):
-        print(n, end=' ')
-    
-    print()
-    
-    for linha in grid:
-        print(linha, end=' ')
-        for coluna in grid[linha]:
-            pos = f'{linha}/{coluna}'
-            
-            if pos in barcos['Carrier']:
-                print('X', end=' ')
-            elif pos in barcos['Dread']:
-                print('X', end=' ')
-            elif pos in barcos['Destroyer']:
-                print('X', end=' ')
-            elif pos in barcos['Sub1']:
-                print('X', end=' ')
-            elif pos in barcos['Sub2']:
-                print('X', end=' ')
-            else:
-                print('~', end=' ')
+def grid_show(funcao):
+    @functools.wraps(funcao)
+    def wrapping(*args, **kwargs):
+        funcao(*args, **kwargs)
+        simbol = ('✖','✦')
+        print('  ', end='')
+        for n in range(1,11):
+            print(n, end=' ')
         print()
+        
+        for linha in grid:
+            print(linha, end=' ')
+            for coluna in range(len(grid[linha])):
+                if grid[linha][coluna] == 0:
+                    pass
+                elif type(grid[linha][coluna]) == int:
+                    print(Back.BLUE + '~',end=Back.BLUE + ' ')
+                elif type(grid[linha][coluna]) == str:
+                    if grid[linha][coluna] == simbol[0]:
+                        print(Fore.YELLOW + grid[linha][coluna], end=Back.BLUE + ' ')
+                    elif grid[linha][coluna] == simbol[1]:
+                        print(Fore.RED + grid[linha][coluna], end=Back.BLUE + ' ')
 
+            print()
+    
+    return wrapping
+
+@grid_show
+def is_in(pos,barcos,grid):
+
+    simbol = ('✖','✦')
+    x, y = pos.split('/')
+    y = int(y)
+
+    ship_hit = False
+
+    for name in barcos:
+        if pos in barcos[name]:
+            ship_hit = True
+            ship_name = name
+    
+    if ship_hit:
+        barcos[ship_name].remove(pos)
+        grid[x][y] = simbol[1]
+    else:
+        grid[x][y] = simbol[0]
+    
+    return grid
+
+    
+def check_sink(barcos):
+
+    doomsday_counter = []
+
+    for nome in barcos:
+        if len(barcos[nome]) == 0:
+            doomsday_counter.append(nome)
+    
+    return doomsday_counter
+
+     
+
+
+    
 
 # ===========================================================================================================
 
 
 barcos = ship_init()
-
 print(barcos)
-Finished = 0
+no_repeat = 0
+Wanna_play = 1
+Partidas = 0
+Record = []
 
-while True:
-    grid_show(grid, barcos)
-    break
+
+while Wanna_play > 0:
+    info = 0
+    play = int(input(Fore.WHITE + 'Quer jogar?\n[1] Sim!\n[0] Não\n'))
+    contador = 0
+
+    if play == 1:
+
+        while True:
+            pos = input('Digite um par de coordenadas nesse modelo "A/5"\n').upper()
+            if '0' in pos and '1' not in pos:
+                print(Fore.RED + 'Posição inválida, por favor tente novamente!')
+                pass
+            else:
+                system('cls')
+                is_in(pos,barcos,grid)
+
+                counter = check_sink(barcos)
+                contador += 1
+
+                if len(counter) == 5:
+                    print(Fore.YELLOW + f'Parabéns soldado, você afundou todos eles em {contador} jogadas!')
+                    break
+                elif len(counter) > no_repeat:
+                    print(Fore.YELLOW + f'Parabéns soldado, já afundou {len(counter)} navio(s) do inimigo')
+                    print(Fore.RED + 'Eram eles')
+                    for barco in counter:
+                        print(Fore.RED + f'{barco}')
+                
+                
+                no_repeat = len(counter)
+            
+    
+    Partidas += 1
+    info = (Partidas, contador)
+    Record.append(info)
+else:
+    print(Fore.WHITE + 'Tudo bem! Aqui está seu histórico')
+    for partida in Record:
+        print(Fore.WHITE + f'Partida {partida[0]} terminou em {partida[1]} jogadas!')
